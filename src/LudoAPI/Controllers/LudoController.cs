4d76c088eae5ffcd1c8c1e19176a557c1bc58741
AnorZaken <AnorZaken@users.noreply.github.com>
2019-02-11 21:15:41 +0100

    Fixed compatibility version, and teapotted!

---
+++ src/LudoAPI/Controllers/LudoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ludo.WebAPI.Controllers
+{
+    [Route("ludo")]
+    [ApiController]
+    public class LudoController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            Response.StatusCode = 418; // I'm a teapot!
+        }
+    }
+}
--- src/LudoAPI/Startup.cs
+++ src/LudoAPI/Startup.cs
@@ -25,7 +25,8 @@ namespace Ludo.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddSingleton<ITODO, TODO>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace Ludo.WebAPI
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
