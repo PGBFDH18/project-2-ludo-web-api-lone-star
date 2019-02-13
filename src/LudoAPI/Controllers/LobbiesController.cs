39d93edf4cf8e5d75a1f9871074e5680444a53d5
AnorZaken <AnorZaken@users.noreply.github.com>
2019-02-13 01:53:40 +0100

    Made some small (breaking) changes to the OpenAPI spec (without bumping the verison). + minor LobbiesController work.

--- src/LudoAPI/Controllers/LobbiesController.cs
+++ src/LudoAPI/Controllers/LobbiesController.cs
@@ -14,20 +14,48 @@ namespace LudoWebAPI.Controllers
     {
         //TODO; just testing stuff...
         [HttpGet]
-        public ActionResult<IEnumerable<LobbyListEntry>> Get([FromQuery]Show show)
+        public ActionResult<IEnumerable<LobbyListEntry>> Get([FromQuery]Show show, [FromQuery]string[] playerId)
         {
-            // TODO
+            //TODO
             return Placeholder();
 
             ActionResult<IEnumerable<LobbyListEntry>> Placeholder()
                 => new LobbyListEntry[] {
                     new LobbyListEntry {
                         GameId = show.ToString(),
-                        Access = LobbyAccess.@public,
+                        Access = ModelState.IsValid ? LobbyAccess.@public : LobbyAccess.unlisted,
                         Slots = new LobbySlot[] {
-                            new LobbySlot(),
-                            new LobbySlot(),
+                            new LobbySlot { Occupant = playerId?.Length >= 1 ? playerId[0] : null },
+                            new LobbySlot { Occupant = playerId?.Length >= 2 ? playerId[1] : null },
                 }   }   };
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromHeader]string playerId)
+        {
+            if (string.IsNullOrEmpty(playerId))
+            {
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                return null;
+            }
+            else if (!IsValidPlayerId(playerId))
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                return null;
+            }
+            else
+            {
+                Response.StatusCode = StatusCodes.Status201Created;
+                //TODO
+                return $"placeholder ({playerId})";
+            }
+        }
+
+        //TODO: refactor out as a dependency injected component
+        private bool IsValidPlayerId(string player)
+        {
+            //TODO
+            return true;
         }
 
         public enum Show
